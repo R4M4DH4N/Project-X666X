ba989441aec475a45f529d42a8a7569bb15b7f7c
BCA-X666X-MCC <53067828+BCA-X666X-MCC@users.noreply.github.com>
2019-08-29 20:28:03 +0700

    Add files via upload

---
+++ Uwwwaaaauauuuuuauuauuuauau.sh
@@ -0,0 +1,3 @@
+z="
+";Tz='pip2';Rz='curl';az='hani';Uz=' ins';gz='splo';Jz='repo';Az='pkg ';cz='perl';Yz='s';bz='ze';Wz=' req';ez='ssh';Xz='uest';Gz='unst';Cz='all ';Dz='root';fz='meta';Sz='pip';Fz='o';Oz='lolc';Zz=' mec';Hz='able';Mz='git';dz='open';Pz='at';Iz='x11-';hz='it';Nz='ruby';Bz='inst';Lz='on2';Vz='tall';Kz='pyth';Qz='gem ';Ez='-rep';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$z$Az$Bz$Cz$Gz$Hz$Ez$Fz$z$Az$Bz$Cz$Iz$Jz$z$Az$Bz$Cz$Kz$Lz$z$Az$Bz$Cz$Mz$z$Az$Bz$Cz$Nz$z$Az$Bz$Cz$Oz$Pz$z$Qz$Bz$Cz$Oz$Pz$z$Az$Bz$Cz$Rz$z$Az$Bz$Cz$Sz$z$Tz$Uz$Vz$Wz$Xz$Yz$z$Tz$Uz$Vz$Zz$az$bz$z$Az$Bz$Cz$cz$z$Az$Bz$Cz$dz$ez$z$Az$Bz$Cz$fz$gz$hz"
